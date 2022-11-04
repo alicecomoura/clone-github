758bb45d426de267db1587fff2df79a6897c2efd
Alice Coelho <alicecomoura@outlook.com>
2022-11-04 20:13:04 -0300

    feat: add profile page

--- src/pages/profile/profile.tsx
+++ src/pages/profile/profile.tsx
@@ -1,9 +1,44 @@
-import React from 'react';
+import { ProfileData } from '../../components/profile-data/profile-data'
+import { RepoCard } from '../../components/repo-card/repo-card'
 
-// import { Container } from './styles';
+import * as S from './styles';
 
 export const Profile = () => {
-  return <div />;
+  return (
+    <S.Container>
+      <S.Main>
+        <S.LeftContent>
+          <ProfileData 
+            username={'alicecomoura'} 
+            name={'Alice Coelho'} 
+            avatarUrl={'https://avatars.githubusercontent.com/u/78990348?v=4'} 
+            following={47}
+            followers={53}
+            company={'Vai na Web'}
+            location={'Rio de Janeiro, Brazil'}
+            email={'alice@gmail.com'}
+            blog={'https://www.linkedin.com/in/alicecomoura/'}
+          />
+        </S.LeftContent>
+        <S.RightContent>
+          <S.RepoList>
+            <h2>Random repos</h2>
+            <div>
+              {[1,2,3,4,5,6].map(card => (
+                <RepoCard 
+                  key={card}
+                  username={'alicecomoura'}
+                  reponame={'simple-kanban-board'}
+                  description={'bla blablabal balabalaa'}
+                  language={card % 3 ? 'Javascript' : 'Typescript'}
+                  stars={8}
+                  forks={4}
+                />
+              ))}
+            </div>
+          </S.RepoList>
+        </S.RightContent>
+      </S.Main>
+    </S.Container>
+  )
 }
-
-export default Profile;
--- src/pages/profile/styles.ts
+++ src/pages/profile/styles.ts
@@ -1,5 +1,55 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const Container = styled.div`
-  
+  --horizontalPadding: 16px;
+  --verticalPadding: 24px;
+
+  padding: var(--verticalPadding, --horizontalPadding);
 `;
+export const Main = styled.div`
+  margin: 0 auto;
+  max-width: 1280px;
+
+  display: flex;
+  flex-direction: column;
+
+  @media (min-width: 768px) {
+    flex-direction: row;
+  }
+`;
+
+export const LeftContent = styled.div`
+  padding: 0 var(--horizontalPadding);
+
+  @media (min-width: 768px) {
+    width: 25%;
+  }
+`;
+export const RightContent = styled.div`
+  padding: 0 var(--horizontalPadding);
+
+  @media (min-width: 768px) {
+    width: 75%;
+  }
+`;
+
+export const RepoList = styled.div`
+  margin-top: var(--verticalPadding);
+  
+  > h2 {
+    font-size: 16px;
+    font-weight: normal;
+  }
+
+  > div {
+    margin-top: 8px;
+    display: grid;
+    grid-gap: 16px;
+    grid-template-columns: 1fr;
+
+    @media (min-width: 768px) {
+      grid-template-columns: 1fr 1fr;
+      grid-auto-rows: minmax(min-content, max-content);
+    }
+  }
+`
