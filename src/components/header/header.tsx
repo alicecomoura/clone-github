ef3b4d0937ba3a729c4e8dd026f22169b79c12e5
Alice Coelho <alicecomoura@outlook.com>
2022-11-05 15:13:20 -0300

    feat: search function

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,11 +1,28 @@
+import { useNavigate } from 'react-router-dom';
+import { useState } from 'react'
+
 import { Container, GithubLogo, SearchForm } from './styles';
 
 export const Header = () => {
+  const [search, setSearch] = useState('')
+
+  const navigate = useNavigate()
+
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault()
+
+    navigate('/' + search.toLowerCase().trim())
+    setSearch('')
+  }
+
   return (
     <Container>
       <GithubLogo />
-      <SearchForm>
-        <input placeholder="Search or jump to.." />
+      <SearchForm onSubmit={handleSubmit}>
+        <input placeholder="Search or jump to.."
+        value={search}
+        onChange={event => setSearch(event.currentTarget.value)}  
+      />
       </SearchForm>
     </Container>
   );
